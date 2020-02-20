343c8cd296e8a06911f04f07abcf2db686477585
majinkun <majinkun@jd.com>
2020-02-20 21:10:28 +0800

    perf(banner): 加banner图和重新打包

--- yolkworks-list/src/views/home/components/banner/banner.vue
+++ yolkworks-list/src/views/home/components/banner/banner.vue
@@ -2,8 +2,8 @@
   <div class="banner">
     <div class="w">
       <div class="banner__inner">
-        <div class="banner__title">海量的模板及组件</div>
-        <h2 class="banner__desc">基于yolkworks进行快速组合搭建应用，拿来即用，持续更新</h2>
+        <!--<div class="banner__title">海量的模板及组件</div>-->
+        <!--<h2 class="banner__desc">基于yolkworks进行快速组合搭建应用，拿来即用，持续更新</h2>-->
       </div>
     </div>
   </div>
